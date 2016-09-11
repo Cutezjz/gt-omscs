a5f26734ab7a5803606b2b18714c643476e0f0f1
Mehmet Bajin <bajin@bajin.lax.corp.google.com>
2016-09-11 08:46:42 -0700

    style: whitespace

--- 6505/test/binary-search-unimodal.spec.js
+++ 6505/test/binary-search-unimodal.spec.js
@@ -10,23 +10,23 @@ describe('binary-search-unimodal', function () {
 
   it('returns index of peak', function () {
     tests = [
-      {array: [1],                expectedIndex: 0},
-      {array: [2],                expectedIndex: 0},
-      {array: [1, 2],             expectedIndex: 1},
-      {array: [2, 1],             expectedIndex: 0},
-      {array: [1, 2, 3],          expectedIndex: 2},
-      {array: [3, 2, 1],          expectedIndex: 0},
-      {array: [1, 3, 2],          expectedIndex: 1},
-      {array: [1, 2, 3, 4],       expectedIndex: 3},
-      {array: [4, 3, 2, 1],       expectedIndex: 0},
-      {array: [1, 4, 3, 2],       expectedIndex: 1},
-      {array: [1, 2, 4, 3],       expectedIndex: 2},
-      {array: [1, 2, 3, 4, 5],    expectedIndex: 4},
-      {array: [5, 4, 3, 2, 1],    expectedIndex: 0},
-      {array: [1, 2, 5, 4, 3],    expectedIndex: 2},
-      {array: [1, 2, 3, 5, 4],    expectedIndex: 3},
-      {array: [1, 5, 4, 3, 2],    expectedIndex: 1},
-      {array: [5, 6, 7, 8, 9, 4], expectedIndex: 4}
+      {array: [1],           expectedIndex: 0},
+      {array: [2],           expectedIndex: 0},
+      {array: [1,2],         expectedIndex: 1},
+      {array: [2,1],         expectedIndex: 0},
+      {array: [1,2,3],       expectedIndex: 2},
+      {array: [3,2,1],       expectedIndex: 0},
+      {array: [1,3,2],       expectedIndex: 1},
+      {array: [1,2,3,4],     expectedIndex: 3},
+      {array: [4,3,2,1],     expectedIndex: 0},
+      {array: [1,4,3,2],     expectedIndex: 1},
+      {array: [1,2,4,3],     expectedIndex: 2},
+      {array: [1,2,3,4,5],   expectedIndex: 4},
+      {array: [5,4,3,2,1],   expectedIndex: 0},
+      {array: [1,2,5,4,3],   expectedIndex: 2},
+      {array: [1,2,3,5,4],   expectedIndex: 3},
+      {array: [1,5,4,3,2],   expectedIndex: 1},
+      {array: [5,6,7,8,9,4], expectedIndex: 4}
     ];
 
     _.forEach(tests, function (test) {
